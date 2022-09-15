d88b83e60be2b8cd885242affcd8740db54ba8ba
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-09-15 10:01:27 +0800

    [fix] allow toblock to be more kinds of interger

--- processToBlock.go
+++ processToBlock.go
@@ -2,21 +2,42 @@ package blockscan
 
 import (
 	"errors"
+	"math/big"
 	"strconv"
 )
 
 func processToBlock(endBlock any) (toBlock string, err error) {
 	switch v := endBlock.(type) {
 	case int:
-		toBlock = strconv.Itoa(v)
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case int8:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case int16:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case int32:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case int64:
+		toBlock = strconv.FormatInt(v, 10)
+	case uint:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case uint8:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case uint16:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case uint32:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case uint64:
+		toBlock = strconv.FormatInt(int64(v), 10)
+	case *big.Int:
+		toBlock = v.String()
 	case string:
 		if v != "latest" {
-			err = errors.New(`endblock should be int or "latest"`)
+			err = errors.New(`endblock should be integer or "latest"`)
 			return
 		}
 		toBlock = "latest"
 	default:
-		err = errors.New(`endblock should be int or "latest"`)
+		err = errors.New(`endblock should be integer or "latest"`)
 		return
 	}
 	return
