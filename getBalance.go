68c4d4ec084c5bfba905f7f38ca1c9434df6c41e
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-28 15:06:06 +0800

    [fix] get balance use big int

--- getBalance.go
+++ getBalance.go
@@ -2,6 +2,7 @@ package blockscan
 
 import (
 	"errors"
+	"math/big"
 
 	"github.com/0xVanfer/blockscan/internal/regularcheck"
 	"github.com/0xVanfer/types"
@@ -10,27 +11,27 @@ import (
 )
 
 // Return the balance of a single address.
-func (s *Scanner) GetBalance(address any) (int64, error) {
+func (s *Scanner) GetBalance(address any) (*big.Int, error) {
 	addressStr, err := regularcheck.RegularCheckAddress(address)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	url := s.UrlHead + `module=account&action=balance&address=` + addressStr + `&tag=latest&apikey=` + s.ApiKey
 	r, err := req.Get(url)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	var res getBalanceReq
 	err = r.ToJSON(&res)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
-	balance := types.ToInt64(res.Result)
+	balance := types.ToBigInt(res.Result)
 	return balance, nil
 }
 
 // Return balances of up to 20 addresses. map[address] = balance.
-func (s *Scanner) GetBalances(addresses []string) (map[string]int64, error) {
+func (s *Scanner) GetBalances(addresses []string) (map[string]*big.Int, error) {
 	addressesString := utils.ConnectArray(addresses, ",")
 	url := s.UrlHead + `module=account&action=balancemulti&address=` + addressesString + `&tag=latest&apikey=` + s.ApiKey
 	r, err := req.Get(url)
@@ -46,9 +47,9 @@ func (s *Scanner) GetBalances(addresses []string) (map[string]int64, error) {
 		err = errors.New(res.Message)
 		return nil, err
 	}
-	resMap := make(map[string]int64)
+	resMap := make(map[string]*big.Int)
 	for _, info := range res.Result {
-		resMap[info.Account] = types.ToInt64(info.Balance)
+		resMap[info.Account] = types.ToBigInt(info.Balance)
 	}
 	return resMap, nil
 }
